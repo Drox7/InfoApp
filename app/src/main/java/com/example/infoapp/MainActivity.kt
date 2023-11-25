fd185a43c715f93cce7055ed741406e87e96f7aa
andres <8891980@gmail.com>
2023-11-25 13:31:34 +0300

    1. added dependencies navigation, 2. added in MainActivity NavHost 3. added object Routes.kt

--- app/src/main/java/com/example/infoapp/MainActivity.kt
+++ app/src/main/java/com/example/infoapp/MainActivity.kt
@@ -3,18 +3,30 @@ package com.example.infoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.infoapp.ui.theme.InfoAppTheme
 import com.example.infoapp.ui_components.MainScreen
+import com.example.infoapp.utils.Routes
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            val navController = rememberNavController()
             InfoAppTheme {
-                MainScreen(context = this)
-
+                NavHost(
+                    navController = navController, 
+                    startDestination = Routes.MAIN_SCREEN
+                ){
+                    composable(Routes.MAIN_SCREEN){
+                   MainScreen(context = this@MainActivity) 
+                }
+                    
+                }
             }
         }
     }
